39bdbb34e388396836b9ee3befaacfa931c8ee9b
Rahul Desai <rd825@georgetown.edu>
2019-04-04 14:48:26 -0400

    Refactored my code  bit & got CALL & RET working

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cpu.h"
 
 /**
@@ -6,10 +7,18 @@
  */
 int main(int argc, char *argv[])
 {
+
+  // Check that filepath is inputted.
+  if (argc < 2)
+  {
+    fprintf(stderr, "Error: no filepath provided.\n");
+    exit(1);
+  }
+
   struct cpu cpu;
 
   cpu_init(&cpu);
-  cpu_load(&cpu, argc, argv[1]); // accept filepath from command line
+  cpu_load(&cpu, argv[1]); // accept filepath from command line
   cpu_run(&cpu);
 
   return 0;
